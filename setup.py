6d8ff44d7b6b746c8d52c3f96d35771a01c213c7
Naim <naim.abdullah@gmail.com>
2023-08-26 16:49:09 -0400

    Update to setup.py with minor changes

--- setup.py
+++ setup.py
@@ -1,18 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='YourPackageName',
+    name='option_toolkit',
     version='0.1.0',
     author='Naim Abdullah',
-    author_email='naim.abdullah@gmail.com',
-    description='A short description of your package',
-    long_description='A longer description or README of your package',
-    url='https://example.com',  # URL for your project's homepage
-    packages=['your_package'],  # List the packages explicitly if needed
-    classifiers=[
-        # ...  # List of classifiers
-    ],
-    install_requires=[
-        # ...  # List of dependencies
-    ],
+    author_email='naim.abdullah@gmail.com'
 )
